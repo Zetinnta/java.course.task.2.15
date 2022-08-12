f216afacc068b223673449b438d31ce42db0eb02
Ян <Shatoshi@mail.ru>
2022-08-12 15:19:44 +0300

    Homework

--- src/main/java/ru/skypro/Main.java
+++ src/main/java/ru/skypro/Main.java
@@ -1,8 +1,25 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        IntegerList integerList = new IntegerListImpl();
+//        integerList.add("line_1");
+//        integerList.add("line_2");
+//        integerList.add("line_3");
+//        integerList.add("line_4");
+//
+//        integerList.add(3, "line_6");
+
+//        integerList.remove(1);
+//
+//        System.out.println(Arrays.toString(integerList.toArray()));
+//
+//        integerList.clear();
+//
+//        System.out.println(integerList.size());
 
     }
 }
